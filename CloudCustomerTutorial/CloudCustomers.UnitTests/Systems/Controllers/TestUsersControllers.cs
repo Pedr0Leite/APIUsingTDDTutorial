4f75f4f55961013ec165359a7beaa8c499955dc7
Pedr0Leite <pedromiguelgoncalvesleite@gmail.com>
2023-09-19 19:25:27 +0100

    firt part from Wes Doyle tutorial

---
+++ CloudCustomerTutorial/CloudCustomers.UnitTests/Systems/Controllers/TestUsersControllers.cs
@@ -0,0 +1,35 @@
+using CloudCustomers.API.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace CloudCustomers.UnitTests.Systems.Controllers;
+
+public class TestUsersControllers
+{
+    [Fact]
+    public async Task Get_OnSuccess_ReturnsStatusCode200()
+    {
+        // Arrange
+        var sut = new UsersController();
+       
+       // Act
+       var result = (OkObjectResult)await sut.Get();
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task Get_OnSuccess_InvokesUserService()
+    {
+        // Arrange
+        var mockUserService = new Mock<IUsersService>();
+        var sut = new UsersController(mockUserService.Object);
+
+        // Act
+        var result = (OkObjectResult)await sut.Get();
+
+        // Assert
+    }
+}
